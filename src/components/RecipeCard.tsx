5da0045030563d625a813fdeb5ff348de99b40e3
Harrison-Mueller <muellerwh@protonmail.com>
2026-05-06 22:51:44 -0400

    Adds Long Name Trimming With CSS And Displays Thumbnails

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -2,6 +2,7 @@ import '../css/RecipeCard.css'
 
 export interface Meal {
     strMeal: string;
+    strMealThumb?: string;
 }
 
 interface Props {
@@ -10,12 +11,12 @@ interface Props {
 
 function RecipeCard({JSON}: Props) {
     if(!JSON ||! JSON.strMeal) {
-        JSON = {strMeal: "Loading..."};
+        JSON = {strMeal: "Loading...", strMealThumb: "ImageLoading.svg"};
     }
 
     return (
         <div className="recipe-card">
-            <img className="recipe-card-image" src="ImageLoading.svg"></img>
+            <img className="recipe-card-image" src={JSON.strMealThumb} alt={JSON.strMeal}></img>
             <h3 className="recipe-card-name">{JSON.strMeal}</h3>
         </div>
     )
