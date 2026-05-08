bdfd8d2058d5d6ce189b76403847fd1212473aef
Harrison-Mueller <muellerwh@protonmail.com>
2026-05-07 20:39:24 -0400

    Adds Basic Recent Queue

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,8 +1,11 @@
 import '../css/RecipeCard.css'
+import { addRecent } from '../services/api';
 
 export interface Meal {
+    idMeal: string;
     strMeal: string;
     strMealThumb?: string;
+
 }
 
 interface Props {
@@ -11,11 +14,23 @@ interface Props {
 
 function RecipeCard({JSON}: Props) {
     if(!JSON ||! JSON.strMeal) {
-        JSON = {strMeal: "Loading...", strMealThumb: "ImageLoading.svg"};
+        JSON = {
+            idMeal: "0", 
+            strMeal: "Loading...", 
+            strMealThumb: "ImageLoading.svg"
+        };
+    }
+    if(!JSON.strMealThumb) {
+        JSON.strMealThumb = "ImageLoading.svg";
+    }
+
+    const handleClick = () => {
+        console.log("Clicked on " + JSON.strMeal);
+        addRecent(JSON as Meal);
     }
 
     return (
-        <div className="recipe-card">
+        <div className="recipe-card" onClick={handleClick}>
             <img className="recipe-card-image" src={JSON.strMealThumb} alt={JSON.strMeal}></img>
             <h3 className="recipe-card-name">{JSON.strMeal}</h3>
         </div>
--- src/services/api.ts
+++ src/services/api.ts
@@ -9,33 +9,72 @@ const delay = (ms: number): Promise<void> => {//simulates delay TEMP
 
 export const getRandom = async (setRecipeList: (recipeJSONList: Meal[]) => void) => {
     setRecipeList(Array.from({ length: 25 }, () => ({} as Meal)));
-    // await delay(4000);
-    let recipeList: Meal[] = [];
+    let recipeList: Meal[] = new Array(25);
     for(let i = 0; i < 25; i++) {
         await fetch(API_URL + "random.php", {method: "POST"})
             .then(function(response) { return response.json(); })
             .then(function(json) {
-              // use the json
-            //   console.log(json);
-            //   console.log(json.meals[0]);
-            //   console.log(json.meals[0].strIngredient2);
-                // recipeList = json.meals;
-                recipeList.push(json.meals[0]);
-            });
-        }
-        setRecipeList(recipeList);
+            recipeList[i] = json.meals[0];
+        });
+    }
+    setRecipeList(recipeList);
     
     // setRecipeList(["APIRandom", "1", "2", "3"]);
 };
 
 export const getFavorites = async (setRecipeList: (recipeJSONList: Meal[]) => void) => {
-    setRecipeList([{strMeal: "APIFavorites"}]);
+    // setRecipeList([{strMeal: "APIFavorites"}]);
 };
 
 export const getRecents = async (setRecipeList: (recipeJSONList: Meal[]) => void) => {
-    setRecipeList([{strMeal: "APIRecents"}]);
+    let jsonRecents: string|null = localStorage.getItem("recentRecipes");
+    if(jsonRecents === null) {
+        setRecipeList([{idMeal: "0", strMeal: "No Recent Recipes"}]);
+        return;
+    }
+    let recents = JSON.parse(jsonRecents);
+    
+    setRecipeList(Array.from({ length: recents.length }, () => ({} as Meal)));
+    let recipeList: Meal[] = new Array(recents.length);
+    for(let i = 0; i < recents.length; i++) {
+        await fetch(API_URL + "lookup.php?i=" + recents[i], {method: "POST"})
+            .then(function(response) { return response.json(); })
+            .then(function(json) {
+            recipeList[recents.length - 1 - i] = json.meals[0];
+        });
+    }
+    setRecipeList(recipeList);
 };
 
 export const searchRecipes = async (setRecipeList: (recipeJSONList: Meal[]) => void) => {
-    setRecipeList([{strMeal: "APISearch"}]);
+    // setRecipeList([{strMeal: "APISearch"}]);
 };
+
+
+export function addRecent(meal: Meal) {
+    if(meal.idMeal === "0" || meal.idMeal == null) {
+        return;
+    }
+    
+    let jsonRecents: string|null = localStorage.getItem("recentRecipes");
+    let recents: string[] = [];
+    if(jsonRecents != null) {
+        recents = JSON.parse(jsonRecents);
+    }
+
+    if(recents.includes(meal.idMeal)) {
+        recents.splice(recents.indexOf(meal.idMeal), 1);
+        recents.push(meal.idMeal);
+    } else {
+        recents.push(meal.idMeal);
+        //Remove down to 25. Loop is used in case 25 is somehow exceeded
+        let excess = recents.length - 25;
+        for(let i = 0; i < excess; i++) {
+            recents.shift();
+        }
+    }
+
+
+
+    localStorage.setItem("recentRecipes", JSON.stringify(recents));
+}
